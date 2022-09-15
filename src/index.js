40f473853d8ad7c4e57c8e54a40fb651901672ee
razhzq <74318704+razhzq@users.noreply.github.com>
2022-09-15 12:54:00 +0800

    database, api

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
 const app = express();  
+app.use(express.json());
+
 // adding Helmet to enhance your Rest API's security
-app.use(helmet());
+// app.use(helmet());
 // using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
 // enabling CORS for all requests
-app.use(cors());
+
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
   
 app.listen(PORT, () => {
   console.log(` Server is up and running on ${PORT} ...`);
--- src/services/approvedLoans.js
+++ src/services/approvedLoans.js
@@ -1,9 +1,11 @@
+const { json } = require("body-parser");
 const express = require("express");
 const config = require('../config/config.js');
 const database = require('../config/db.js');
 const databaseName = config.database;
 
 var loanRouter = express.Router();
+
 
 // Create user table
 loanRouter.get("/", (req, res) => {
@@ -16,8 +18,50 @@ loanRouter.get("/", (req, res) => {
         //Change loan_status if needed
         let getApprovedLoans = "SELECT * FROM loans where loan_status=1";
         database.query(getApprovedLoans, (error,results) => {
-            if (error) 
-                throw error;
+            if (error) {
+                console.log(error.status);
+            }
+                
+            return res.status(200).json(results);        
+        });
+    })
+});
+
+
+loanRouter.get("/contracts", (req, res) => {
+    let useQuery = `USE ${databaseName}`;
+    database.query(useQuery, (error) => {
+        if (error) 
+            throw error;
+        
+        console.log("Using Database");
+        //Change loan_status if needed
+        let getLoanContracts = "SELECT * FROM loan_contracts";
+        database.query(getLoanContracts, (error,results) => {
+            if (error) {
+                console.log(error.status);
+            }
+                
+            return res.status(200).json(results);        
+        });
+    })
+});
+
+loanRouter.post("/contracts", (req, res) => {
+    let useQuery = `USE ${databaseName}`;
+    let loanContract = req.body;
+    database.query(useQuery, (error) => {
+        if (error) 
+            throw error;
+        
+        console.log("Using Database");
+        //Change loan_status if needed
+        let postLoanContracts = "INSERT INTO loan_contracts  (`loan_id`, `loan_contract_address`) VALUES ('"+loanContract.loan_id+"', '"+loanContract.loan_contract_address+"')";
+        database.query(postLoanContracts, (error,results) => {
+            if (error) {
+                console.log(error.status);
+            }
+                
             return res.status(200).json(results);        
         });
     })
